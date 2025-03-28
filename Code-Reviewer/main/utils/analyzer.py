8ecccf247862aa7c089c44f3af05a22f9cccc48b
Gaurav-714 <gauravnagose714@gmail.com>
2025-03-28 14:36:46 +0530

    exception handling

--- Code-Reviewer/main/utils/analyzer.py
+++ Code-Reviewer/main/utils/analyzer.py
@@ -6,21 +6,25 @@ from .prompts import get_prompts
 
 def analyze_code_with_llm(file_name, file_content):
     system_prompt, user_prompt = get_prompts(file_name, file_content)
+    try:
+        api_key = "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"
+        client = Groq(api_key=api_key)
 
-    api_key = "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"
-    client = Groq(api_key=api_key)
-
-    completion = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[
-            {'role': 'system', 'content': system_prompt},
-            {'role': 'user', 'content': user_prompt}
-        ],
-        temperature=0.7,
-        top_p=0.9,
-        response_format={"type": "json_object"},
-    )
-    return completion.choices[0].message.content
+        completion = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {'role': 'system', 'content': system_prompt},
+                {'role': 'user', 'content': user_prompt}
+            ],
+            temperature=0.7,
+            top_p=0.9,
+            response_format={"type": "json_object"},
+        )
+        return completion.choices[0].message.content
+    
+    except Exception as ex:
+        print(ex)
+        return None
 
 
 def pr_analysis(repo_url, pr_number, github_token):
--- Code-Reviewer/main/views.py
+++ Code-Reviewer/main/views.py
@@ -1,27 +1,53 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 from celery.result import AsyncResult
 from .tasks import pr_analysis_task
 
 
 class AnalyzePullRequest(APIView):
     def post(self, request):
-        data = request.data 
-        repo_url = data.get("repo_url")
-        pr_number = data.get("pr_number")
-        github_token = data.get("github_token")
-        task_result = pr_analysis_task(repo_url, pr_number, github_token)
+        try:
+            data = request.data 
+            repo_url = data.get("repo_url")
+            pr_number = data.get("pr_number")
+            github_token = data.get("github_token")
+            task_result = pr_analysis_task(repo_url, pr_number, github_token)
 
-        return Response({
-            "analysis_id": task_result.id,
-            "status": "Started Analyzing Pull Request"
-        })
+            return Response({
+                "success": True,
+                "analysis_id": task_result.id,
+                "status": "Started Analyzing Pull Request"
+            }, status=status.HTTP_102_PROCESSING)
+        
+        except Exception as ex:
+            return Response({
+                "success": False,
+                "message": "something went wrong",
+                "details": str(ex)
+            })
     
 
 class AnalysisStatus(APIView):
     def get(self, request, analysis_id):
-        result = AsyncResult(analysis_id)
-        return Response({
-            "analysis_id": analysis_id,
-            "status": result.state
-        })
+        try:
+            result = AsyncResult(analysis_id)
+            response_data = {
+                'analysis_id': analysis_id,
+                'status': result.state,
+            }
+            if result.state == 'SUCCESS':
+                response_data['result'] = result.result
+            elif result.state == 'FAILURE':
+                response_data['error'] = str(result.result)
+
+            return JsonResponse(response_data)
+        
+        except Exception as ex:
+            return Response({
+                "success": False,
+                "message": "something went wrong",
+                "details": str(ex)
+            })
+    
