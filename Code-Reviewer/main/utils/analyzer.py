f095b8f073d2cf24c52fa6d4218cda479823328a
Gaurav-714 <gauravnagose714@gmail.com>
2025-03-29 01:58:42 +0530

    modifications

--- Code-Reviewer/main/utils/analyzer.py
+++ Code-Reviewer/main/utils/analyzer.py
@@ -51,10 +51,11 @@ def pr_analysis(repo_url, pr_number, github_token):
 
         for file in pr_files:
             file_name = file['filename']
+            print(f"*** Processing file: {file_name} ***")
             file_content = fetch_file_content(repo_url, file_name, github_token)
 
             if "__pycache__" in file_name or file_name.endswith(".pyc"):
-                print(f"Skipping cached file: {file_name}")
+                print(f"*** Skipping cached file: {file_name} ***")
                 continue  
             
             file_analysis = analyze_code_with_llm(file_name, file_content)
