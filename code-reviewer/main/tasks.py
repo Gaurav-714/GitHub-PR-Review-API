56e4a583b4d2e0fda8185e17dda3cb3a8e828479
Gaurav-714 <gauravnagose714@gmail.com>
2025-03-29 15:55:15 +0530

    ready with development part

---
+++ code-reviewer/main/tasks.py
@@ -0,0 +1,15 @@
+from celery import Celery, shared_task
+from .utils.analyzer import pr_analysis
+
+app = Celery("core")
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+@shared_task
+def pr_analysis_task(repo_url: str, pr_number: int, github_token: str = None):
+    result = pr_analysis(repo_url, pr_number, github_token)
+
+    if hasattr(result, "values"):  
+        result = list(result.values()) 
+
+    return result
+    
