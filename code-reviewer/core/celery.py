a9c0ec529afea547a8d1dff72980caab494f4de9
Gaurav-714 <gauravnagose714@gmail.com>
2025-03-31 01:08:36 +0530

    update celery configuration

--- code-reviewer/core/celery.py
+++ code-reviewer/core/celery.py
@@ -4,6 +4,16 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('core')
+
+broker_url = os.getenv("CELERY_BROKER_URL")
+result_backend = os.getenv("CELERY_RESULT_BACKEND")
+
+if not broker_url or not result_backend:
+    raise ValueError("CELERY_BROKER_URL or CELERY_RESULT_BACKEND is not set properly!")
+
+app.conf.broker_url = broker_url
+app.conf.result_backend = result_backend
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
--- code-reviewer/main/tasks.py
+++ code-reviewer/main/tasks.py
@@ -1,9 +1,6 @@
 from celery import Celery, shared_task
 from .utils.analyzer import pr_analysis
 
-app = Celery("core")
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
 @shared_task
 def pr_analysis_task(repo_url: str, pr_branch: str, pr_number: int, github_token: str = None):
     result = pr_analysis(repo_url, pr_branch, pr_number, github_token)
